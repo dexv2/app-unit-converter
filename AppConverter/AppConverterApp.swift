855c5ce6a60c19b42a2280ec192a86bf3a5617b1
phil002109 <vermontphil.paguiligan@gmail.com>
2024-09-04 10:42:22 +0800

    Initial Commit

---
+++ AppConverter/AppConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppConverterApp.swift
+//  AppConverter
+//
+//  Created by Vermont Phil Paguiligan on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct AppConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
